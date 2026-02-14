2b2227cd01847b9204c5d6ba9f1021ed85918fcb
Ruslan <randomrocketgalaxy@gmail.com>
2026-02-14 21:27:33 +0200

    ADD api fetch util

---
+++ utils/index.ts
@@ -0,0 +1,15 @@
+export async function fetchCars() {
+    const headers = {
+            'x-rapidapi-key': '6fe272275fmsh1a771261bfdf3a7p1c79aajsn8a1969b48a2d',
+            'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+        }
+
+    const response = await fetch(`const url = 'https://cars-by-a
+        pi-ninjas.p.rapidapi.com/v1/cars?model=corolla&min_comb_mpg=2';`, { 
+        headers: headers,
+        });
+
+    const result = await response.json();
+
+    return result;
+}
