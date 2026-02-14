52724797b1401a992a8f23367fb0e3541868abb5
Ruslan <randomrocketgalaxy@gmail.com>
2026-02-14 18:34:49 +0200

    ADD initial searchbar components and types

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import { Hero } from "@/components";
+import { SearchBar, CustomFilter } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="padding-x mt-12 padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className="home__filters">
+          <SearchBar />
+          
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+
+        </div>
+      </div>
     </main>
   );
 }
